ca466defa2b75f9593f65822501e89c64596103d
Yonatan <yonatan62862@gmail.com>
2025-01-12 16:23:23 +0200

    Cors

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,13 @@ db.once("open", () => console.log("Connected to Database"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.use("/posts", postsRoutes);
 app.use("/comments", commentsRoutes);
 app.use("/auth", authRoutes);
