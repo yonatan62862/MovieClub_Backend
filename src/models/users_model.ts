0e157de3aa343237a4fa37daa4becbd8510bd1fc
Yonatan <yonatan62862@gmail.com>
2025-03-18 19:59:28 +0200

    Refactor

--- src/models/users_model.ts
+++ src/models/users_model.ts
@@ -2,21 +2,12 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 export interface IUser {
-<<<<<<< HEAD
-=======
   firstName: string;
   lastName: string;
->>>>>>> server_branch
   email: string;
   password: string;
   _id?: string;
   refreshToken?: string[];
-<<<<<<< HEAD
-  avatar?: string;
-}
-
-const userSchema = new Schema<IUser>({
-=======
   profilePicture?: string;
 }
 
@@ -29,7 +20,6 @@ const userSchema = new Schema<IUser>({
     type: String,
     required: true,
   },
->>>>>>> server_branch
   email: {
     type: String,
     required: true,
@@ -43,23 +33,14 @@ const userSchema = new Schema<IUser>({
     type: [String],
     default: [],
   },
-<<<<<<< HEAD
-  avatar: {
-    type: String,
-=======
   profilePicture: {
     profilePicture: {
       type: String,
       default: "https://via.placeholder.com/150",
     },
->>>>>>> server_branch
   },
 });
 
 const userModel = mongoose.model<IUser>("Users", userSchema);
 
-<<<<<<< HEAD
 export default userModel;
-=======
-export default userModel;
->>>>>>> server_branch
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import express, { Express } from "express";
-const app = express();
-import dotenv from "dotenv";
-dotenv.config();
-import mongoose from "mongoose";
-import bodyParser from "body-parser";
-import postsRoutes from "./routes/posts_routes";
-import commentsRoutes from "./routes/comments_routes";
-import authRoutes from "./routes/auth_routes";
-import swaggerJsDoc from "swagger-jsdoc";
-import swaggerUI from "swagger-ui-express";
-import likesRoutes from "./routes/likes_routes";
-import cors from "cors";
-
-
-=======
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -33,7 +16,6 @@ const app = express();
 dotenv.config();
 
 app.use(passport.initialize());
->>>>>>> server_branch
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
@@ -41,10 +23,7 @@ db.once("open", () => console.log("Connected to Database"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-<<<<<<< HEAD
-=======
 app.use(express.json());
->>>>>>> server_branch
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -55,20 +34,11 @@ app.use((req, res, next) => {
 
 app.use(cors());
 
-<<<<<<< HEAD
-
-=======
->>>>>>> server_branch
 app.use("/posts", postsRoutes);
 app.use("/comments", commentsRoutes);
 app.use("/auth", authRoutes);
 app.use("/likes", likesRoutes);
-<<<<<<< HEAD
-
-
-=======
 app.use("/users", userRoutes);
->>>>>>> server_branch
 
 const options = {
   definition: {
@@ -78,36 +48,22 @@ const options = {
       version: "1.0.0",
       description: "REST server including authentication using JWT",
     },
-<<<<<<< HEAD
-    servers: [{ url: "http://localhost:" + process.env.PORT, },],
-=======
     servers: [{ url: "http://localhost:" + process.env.PORT }],
->>>>>>> server_branch
   },
   apis: ["./src/routes/*.ts"],
 };
 const specs = swaggerJsDoc(options);
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
-<<<<<<< HEAD
-
-=======
->>>>>>> server_branch
 const initApp = () => {
   return new Promise<Express>((resolve, reject) => {
     if (process.env.DB_CONNECTION == undefined) {
       reject("DB_CONNECTION is not defined");
     } else {
       mongoose
-<<<<<<< HEAD
-      .connect(process.env.DB_CONNECTION)
-      .then(() => resolve(app))
-      .catch((error) => reject(error));
-=======
         .connect(process.env.DB_CONNECTION)
         .then(() => resolve(app))
         .catch((error) => reject(error));
->>>>>>> server_branch
     }
   });
 };
