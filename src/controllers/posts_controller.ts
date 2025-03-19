26afd3cd8acef01c52bf97bef98117801bde9232
Bar Kobi <122670126+barkobi40@users.noreply.github.com>
2025-03-19 18:07:42 +0200

    Fixed all controllers added gemini controller

--- src/controllers/posts_controller.ts
+++ src/controllers/posts_controller.ts
@@ -4,27 +4,26 @@ import commentModel from "../models/comments_model";
 import BaseController from "./base_controller";
 
 class PostController extends BaseController<IPost> {
-    constructor() {
-        super(postModel);
-    }
+  constructor() {
+    super(postModel);
+  }
 
-   
-async create(req: Request, res: Response) {
-      const userId = req.params.userId;
-      const post = {
-          ...req.body,
-          owner: userId
-      }
-      req.body = post;
-      super.create(req, res);
-  };
+  async create(req: Request, res: Response) {
+    const userId = req.params.userId;
+    
+    const post = {
+      ...req.body,
+      owner: userId,
+    };
+    req.body = post;
+    super.create(req, res);
+  }
 
-async deleteItem(req: Request, res: Response) {
+  async deleteItem(req: Request, res: Response) {
     const postId = req.params.id;
     await commentModel.deleteMany({ postId });
-    super.deleteItem(req, res); 
-    }
+    super.deleteItem(req, res);
+  }
 }
 
 export default new PostController();
-
